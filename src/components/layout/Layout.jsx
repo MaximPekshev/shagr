c8752ed25876a23e6d0bd2973e58d312706c4e50
MaximPekshev <mp198769@gmail.com>
2025-12-18 10:02:18 +0600

    Header component separate from Layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,100 +1,15 @@
-import { Outlet, NavLink, useLocation } from "react-router";
-import { Flex, Layout, Badge } from 'antd';
-import { Menu } from 'antd';
+import { Outlet } from "react-router";
+import { Flex, Layout } from 'antd';
 import styles from './layout.module.css';
-import logo from '../../assets/img/logo/logo.png';
-import { MenuOutlined } from '@ant-design/icons';
-import { MobileMenu } from "../mobileMenu/MobileMenu";
-import { useState } from "react";
-import { ShoppingCartOutlined, HeartOutlined } from '@ant-design/icons';
-import { useGetCartQuery } from "../../redux/services/cart";
-import { useGetWishlistQuery } from "../../redux/services/wishlist";
+import { HeaderComponent } from "../header/Header";
 
-const { Header, Footer, Content } = Layout;
+const { Footer, Content } = Layout;
 
 export const MainLayout = () => {
-    const token = localStorage.getItem('shagr_token');
-    const location = useLocation();
-    const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
-    const { data: cart } = useGetCartQuery({header: { token: token }});
-    const { data: wishlist } = useGetWishlistQuery({header: { token: token }});
-
-    const getCurrentKey = (path) => {
-        if (path === '/') return '1';
-        if (path === '/catalog') return '2';
-        if (path === '/contacts') return '3';
-        return '';
-    };
-
-    const currentKey = getCurrentKey(location.pathname);
-
-    const toggleMobileMenu = () => {
-        setMobileMenuVisible(!mobileMenuVisible);
-    };
-
-    const Logout = () => {
-        localStorage.removeItem('shagr_token');
-        window.location.reload();
-    };
-
     return (
         <Flex gap="middle" wrap>
             <Layout className={styles.layout}>
-                <Header className={styles.header}>
-                    <div className={styles.logo}>
-                        <NavLink to="/">
-                            <img src={logo} alt="Logo" />
-                        </NavLink>
-                    </div>
-                    <Menu
-                        className={styles.menu}
-                        mode="horizontal"
-                        selectedKeys={[currentKey]}
-                        items={[
-                        { key: '1', label: (
-                            <NavLink to="/">
-                                Home
-                            </NavLink>
-                            ) },
-                        { key: '2', label: (
-                            <NavLink to="/catalog">
-                                Catalog
-                            </NavLink>
-                            ) },
-                        { key: '3', label: (
-                            <NavLink to="/contacts">
-                                Contacts
-                            </NavLink>
-                            ) },
-                        ]}
-                    />
-                    <div className={styles.userActions}>
-                        { token ? (
-                            <>
-                                <NavLink to="/account" className={styles.loginButton}>Account</NavLink>
-                                <button onClick={Logout} className={styles.loginButton}>Logout</button>
-                            </>
-                        ) : (
-                            <NavLink to="/login" className={styles.loginButton}>Login</NavLink>
-                        )}
-                        <Badge size="small" count={ cart ? cart.items.length : 0 }>
-                            <NavLink to="/cart" className={styles.cart}>
-                                <ShoppingCartOutlined className={styles.cartIcon} />
-                            </NavLink>
-                        </Badge>
-                        <Badge size="small" count={ wishlist ? wishlist.items.length : 0 }>
-                            <NavLink to="/wishlist" className={styles.wishlist}>
-                                <HeartOutlined className={styles.wishIcon} />
-                            </NavLink>
-                        </Badge>
-                    </div>
-                    <div className={styles.hamburgerMenu}>
-                        <button onClick={() => toggleMobileMenu()} className={styles.mobileMenuButton}>
-                            <MenuOutlined />
-                        </button>
-                    </div>
-                    <MobileMenu visible={mobileMenuVisible} onClose={toggleMobileMenu} />
-                </Header>
+                <HeaderComponent />
                 <Content className={styles.content}>
                     <Outlet />
                 </Content>
